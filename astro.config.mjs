a27f0dd51d3de68ffe0c2475be02abf6ca485b52
Sugumaran <141924234+sugumaran-nix@users.noreply.github.com>
2026-06-21 08:51:25 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://sugumaran-portfolio.vercel.app',
   integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
